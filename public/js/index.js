bac02899d3361abfcc28ad0fe670a15e82d06b00
Nathan2401 <oluwolefalasinu@gmail.com>
2021-04-17 21:40:51 +0100

    re-add files

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-const allPage = ()=>{
+
 
     /* Begining of Form Page */
 
@@ -11,7 +11,7 @@ const formPage = () => {
     
     
     investAmnt.addEventListener('keypress',(e)=>{
-    let investAmountPercent = ((e.target.value/100000000)*100)
+    let investAmountPercent = ((e.target.value/10000000)*100)
     if(investAmntVal===""||null){
         document.getElementById('ev-color').textContent ='0%';
         
@@ -44,76 +44,13 @@ const formPage = () => {
     }
     formPage()
     
-    /**Getting The Values */
-
-    const getValues = ()=>{
-        const fullname = localStorage.getItem('fullname');
-        const investAmnt = localStorage.getItem('investAmnt');
-        const lastMonthRev = localStorage.getItem('lastMonthRev');
-        const lastMonthExp = localStorage.getItem('lastMonthExp');
-        const amountPercent = localStorage.getItem('amountPercent');
-        const defaultWorth = 100000000;
-        const defaultProfitBalance = 40000;
-        companyValue = parseInt(defaultWorth) + parseInt(lastMonthRev) + parseInt(investAmnt);
-        
-        
-        const percentWorth = parseInt(amountPercent)* companyValue;
-        lastMonthProfit = lastMonthRev - lastMonthExp;
-        
-        
-        percentMonthProfit = parseInt(amountPercent)*lastMonthProfit;
-        
-        const accruedProfit = defaultProfitBalance + percentMonthProfit;
-        
-        const annualProfit = percentMonthProfit*12;
     
-        postDay = new Date(Date.now()).toDateString();
-        today = new Date().toDateString();
-    
-    
-        const downloadButton =document.getElementById('download-btn')
-    
-        /* appending values to page*/
-        
-        
-        document.getElementById('company-value').textContent = `₦${companyValue}`;
-        document.getElementById('company-revenue').textContent = `₦${lastMonthRev}`;
-        document.getElementById('company-expense').textContent = `₦${lastMonthExp}`;
-        
-        document.getElementById('company-profit').textContent = `₦${lastMonthProfit}`;
-        document.getElementById('full-name').textContent =fullname;
-        document.getElementById('amount-invested').textContent = `₦${investAmnt}`;
-        document.getElementById('percent-worth').textContent = `₦${percentWorth}`;
-        document.getElementById('amount-percent').textContent = amountPercent;
-        document.getElementById('month-profit').textContent = `₦${percentMonthProfit}`;
-        document.getElementById('accrued-profit').textContent = `₦${accruedProfit}`;
-        document.getElementById('annual-profit').textContent = `₦${annualProfit}`;
-        document.getElementById('investment-date').textContent =postDay;
-        document.getElementById('maturity-date').textContent = today;
-        downloadButton.addEventListener('click',()=>{
-            window.print();
-        })
-        
-        
-        console.log(postDay,today);
-    
-        
-        
-        
-        console.log(localStorage);
-        
-    }
-    getValues();
 
 
 
 
 
 
-
-}
-
-allPage();
 
         
 
--- public/js/mad.js
+++ public/js/mad.js
@@ -1,3 +1,5 @@
+/**Getting The Values */
+
 const getValues = ()=>{
     const fullname = localStorage.getItem('fullname');
     const investAmnt = localStorage.getItem('investAmnt');
@@ -6,18 +8,21 @@ const getValues = ()=>{
     const amountPercent = localStorage.getItem('amountPercent');
     const defaultWorth = 100000000;
     const defaultProfitBalance = 40000;
-    companyValue = parseInt(defaultWorth) + parseInt(lastMonthRev) + parseInt(investAmnt);
+    companyValue = ((parseInt(defaultWorth) + parseInt(lastMonthRev) + parseInt(investAmnt))/1000000);
+    const investAmntString = investAmnt/1000000;
     
     
-    const percentWorth = parseInt(amountPercent)* companyValue;
-    lastMonthProfit = lastMonthRev - lastMonthExp;
+    const percentWorth = ((parseInt(amountPercent)* companyValue)/1000000);
+    lastMonthProfit = ((lastMonthRev - lastMonthExp)/1000000);
+    lastMonthExpString = lastMonthExp/1000000 ;
+    lastMonthRevString = lastMonthRev/1000000 ;
     
     
-    percentMonthProfit = parseInt(amountPercent)*lastMonthProfit;
+    percentMonthProfit = ((parseInt(amountPercent)*lastMonthProfit)/1000000).toFixed(5);
     
-    const accruedProfit = defaultProfitBalance + percentMonthProfit;
+    const accruedProfit = ((defaultProfitBalance + percentMonthProfit)/1000000).toFixed(5);
     
-    const annualProfit = percentMonthProfit*12;
+    const annualProfit = ((percentMonthProfit*12)/1000000).toFixed(5);
 
     postDay = new Date(Date.now()).toDateString();
     today = new Date().toDateString();
@@ -28,18 +33,18 @@ const getValues = ()=>{
     /* appending values to page*/
     
     
-    document.getElementById('company-value').textContent = `₦${companyValue}`;
-    document.getElementById('company-revenue').textContent = `₦${lastMonthRev}`;
-    document.getElementById('company-expense').textContent = `₦${lastMonthExp}`;
+    document.getElementById('company-value').textContent = `₦${companyValue}M`;
+    document.getElementById('company-revenue').textContent = `₦${lastMonthRevString}M`;
+    document.getElementById('company-expense').textContent = `₦${lastMonthExpString}M`;
     
-    document.getElementById('company-profit').textContent = `₦${lastMonthProfit}`;
-    document.getElementById('fullname').textContent =  fullname;
-    document.getElementById('amount-invested').textContent = `₦${investAmnt}`;
-    document.getElementById('percent-worth').textContent = `₦${percentWorth}`;
+    document.getElementById('company-profit').textContent = `₦${lastMonthProfit}M`;
+    document.getElementById('full-name').textContent =fullname;
+    document.getElementById('amount-invested').textContent = `₦${investAmntString}M`;
+    document.getElementById('percent-worth').textContent = `₦${percentWorth}M`;
     document.getElementById('amount-percent').textContent = amountPercent;
-    document.getElementById('month-profit').textContent = `₦${percentMonthProfit}`;
-    document.getElementById('accrued-profit').textContent = `₦${accruedProfit}`;
-    document.getElementById('annual-profit').textContent = `₦${annualProfit}`;
+    document.getElementById('month-profit').textContent = `₦${percentMonthProfit}M`;
+    document.getElementById('accrued-profit').textContent = `₦${accruedProfit}M`;
+    document.getElementById('annual-profit').textContent = `₦${annualProfit}M`;
     document.getElementById('investment-date').textContent =postDay;
     document.getElementById('maturity-date').textContent = today;
     downloadButton.addEventListener('click',()=>{
@@ -56,3 +61,4 @@ const getValues = ()=>{
     
 }
 getValues();
+
